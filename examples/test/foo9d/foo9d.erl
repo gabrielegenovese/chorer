78e68726393f6b85550d8015d65464e62719ed78
German Vidal <gvidal@dsic.upv.es>
2024-06-12 00:09:11 +0200

    Minor change

--- examples/test/foo9d/foo9d.erl
+++ examples/test/foo9d/foo9d.erl
@@ -1,16 +1,16 @@
 -module(foo9d).
 -export([main/0,f/2,foo/0]).
-
+ 
 main() -> 
     S = spawn(foo9d,foo,[]),
     f(S,3).
-
+ 
 f(S,N) -> case N of
-            0 -> S ! 0;
-            M when M>0 -> f(S,N-1), S ! M 
+            0 -> S ! stop;
+            M when M>0 -> f(S,M-1), S ! msg 
           end.
-
+ 
 foo() -> receive
-            ok -> done;
+            stop-> done;
             _ -> foo()
          end.
